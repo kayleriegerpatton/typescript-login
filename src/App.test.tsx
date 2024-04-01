828319fb2ccbfbe7b3112f3b6b4aa0b2b099e972
Kayle Rieger Patton <kayle.patton22@gmail.com>
2024-04-01 14:59:37 +0200

    updated default test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders form title", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Log in/);
   expect(linkElement).toBeInTheDocument();
 });
